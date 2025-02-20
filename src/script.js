1f4d6bf97b8571f701963794e13c26ffea290e45
Elijah Won <ejinsw@gmail.com>
2025-02-19 19:57:26 -0800

    Add terrarium box w/ transformable objects

--- src/script.js
+++ src/script.js
@@ -14,6 +14,7 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+scene.background = new THREE.Color('#87ceeb')
 
 // Raycaster
 const raycaster = new THREE.Raycaster()
@@ -25,26 +26,108 @@ const mouse = new THREE.Vector2()
 const textureLoader = new THREE.TextureLoader()
 
 /**
+ * Terrarium
+ */
+const terrariumDimensions = {
+    width: 15,
+    height: 10,
+    depth: 15
+}
+
+gui.add(terrariumDimensions, 'width').min(1).max(20).step(0.1).onChange(() => {
+    ground.geometry.dispose()
+    ground.geometry = new THREE.BoxGeometry(terrariumDimensions.width, terrariumDimensions.height, terrariumDimensions.depth)
+
+    container.geometry.dispose()
+    container.geometry = new THREE.BoxGeometry(terrariumDimensions.width, terrariumDimensions.height, terrariumDimensions.depth)
+})
+gui.add(terrariumDimensions, 'height').min(1).max(20).step(0.1).onChange(() => {
+    ground.geometry.dispose()
+    ground.geometry = new THREE.BoxGeometry(terrariumDimensions.width, terrariumDimensions.height, terrariumDimensions.depth)
+
+    ground.position.y = - terrariumDimensions.height / 2
+
+    container.geometry.dispose()
+    container.geometry = new THREE.BoxGeometry(terrariumDimensions.width, terrariumDimensions.height, terrariumDimensions.depth)
+
+    container.position.y = terrariumDimensions.height / 2
+
+})
+gui.add(terrariumDimensions, 'depth').min(1).max(20).step(0.1).onChange(() => {
+    ground.geometry.dispose()
+    ground.geometry = new THREE.BoxGeometry(terrariumDimensions.width, terrariumDimensions.height, terrariumDimensions.depth)
+
+    container.geometry.dispose()
+    container.geometry = new THREE.BoxGeometry(terrariumDimensions.width, terrariumDimensions.height, terrariumDimensions.depth)
+})
+
+const terrarium = new THREE.Group()
+
+const ground = new THREE.Mesh(
+    new THREE.BoxGeometry(terrariumDimensions.width, terrariumDimensions.height, terrariumDimensions.depth),
+    new THREE.MeshPhongMaterial({ color: 'lightgreen' })
+)
+ground.position.y = - terrariumDimensions.height / 2
+ground.receiveShadow = true
+terrarium.add(ground)
+
+const container = new THREE.Mesh(
+    new THREE.BoxGeometry(terrariumDimensions.width, terrariumDimensions.height, terrariumDimensions.depth),
+    new THREE.MeshPhongMaterial({ color: '#c7f5fb', transparent: true, opacity: 0.2, shininess: 100 })
+)
+container.position.y = terrariumDimensions.height / 2
+terrarium.add(container)
+
+terrarium.position.y = - 3
+
+scene.add(terrarium)
+
+/**
  * Objects
  */
-const objects = [
-    new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({ color: 0xff0000 })
-    ),
-    new THREE.Mesh(
-        new THREE.SphereGeometry(1, 16, 16),
-        new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-    ),
-    new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({ color: 0x0000ff })
+function makeObject(geometry, material) {
+    const object = new THREE.Mesh(
+        geometry,
+        material
     )
+
+    object.position.x = (Math.random() - 0.5) * 10
+    object.position.y = Math.random() * 5
+    object.position.z = (Math.random() - 0.5) * 10
+    object.rotation.x = Math.random() * Math.PI
+
+    object.castShadow = true
+    object.material.shininess = 100
+    object.material.roughness = 0.5
+    object.material.metalness = 0.5
+
+
+    return object
+}
+
+const objects = [
+    makeObject(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshPhongMaterial({ color: 'gray' })),
+    makeObject(new THREE.SphereGeometry(1, 16, 16), new THREE.MeshPhongMaterial({ color: 'blue' })),
+    makeObject(new THREE.ConeGeometry(1, 2, 16), new THREE.MeshPhongMaterial({ color: 'red' })),
 ]
 
 objects.forEach((object) => {
+    object.geometry.computeBoundingBox()
     scene.add(object)
 })
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 20, 20, 2)
+pointLight.castShadow = true
+pointLight.position.x = 0
+pointLight.position.y = 3
+pointLight.position.z = 0
+scene.add(pointLight)
 
 /**
  * Sizes
@@ -73,7 +156,7 @@ window.addEventListener('resize', () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0, 3, 8)
+camera.position.set(0, 5, 15)
 scene.add(camera)
 
 /**
@@ -84,15 +167,34 @@ const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
 // Transform Controls
+let prevPosition = new THREE.Vector3()
 const transformControls = new TransformControls(camera, canvas)
 transformControls.addEventListener('dragging-changed', (event) => {
     controls.enabled = !event.value
+    prevPosition.copy(transformControls.object.position)
 })
+
+transformControls.addEventListener('change', () => {
+    const boundingBox = new THREE.Box3().setFromObject(transformControls.object)
+    const dimensions = new THREE.Vector3()
+    boundingBox.getSize(dimensions)
+
+    let clampedPosition = new THREE.Vector3().copy(transformControls.object.position)
+
+    clampedPosition.x = Math.min(Math.max(clampedPosition.x, - terrariumDimensions.width / 2 + dimensions.x / 2), terrariumDimensions.width / 2 - dimensions.x / 2)
+    clampedPosition.y = Math.min(Math.max(clampedPosition.y, terrarium.position.y + dimensions.y / 2), terrariumDimensions.height + terrarium.position.y - dimensions.y / 2)
+    clampedPosition.z = Math.min(Math.max(clampedPosition.z, - terrariumDimensions.depth / 2 + dimensions.z / 2), terrariumDimensions.depth / 2 - dimensions.z / 2)
+
+    transformControls.object.position.copy(clampedPosition)
+}
+)
+
 scene.add(transformControls)
 
 objects.forEach((object, index) => {
     object.addEventListener('click', () => {
         transformControls.attach(object)
+        prevPosition.copy(object.position)
     })
 })
 
@@ -112,6 +214,14 @@ window.addEventListener('click', (event) => {
         const object = intersects[0].object
 
         transformControls.attach(object)
+    }
+})
+
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'r') {
+        transformControls.mode = transformControls.mode === 'translate' ? 'rotate'
+            : transformControls.mode === 'rotate' ? 'scale'
+                : 'translate'
     }
 })
 
